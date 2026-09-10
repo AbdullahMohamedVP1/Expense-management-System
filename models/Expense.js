640f9a2e25806c8a054aab59a259af143c733828
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-09-10 20:45:24 +0300

    Add expense model

---
+++ models/Expense.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+    category: { type: String },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Expense", expenseSchema);
