7abb3273a4747c522abb3d8be2ca5cd93a827b96
Abdullah_Mohamed_ِAbdelhaq <boda12834@gmail.com>
2026-09-12 13:41:22 +0300

    Delete models/Income.js Updated

--- models/Income.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const incomeSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    amount: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-    category: { type: String },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Income", incomeSchema);
