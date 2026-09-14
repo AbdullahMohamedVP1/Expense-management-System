1c11fb7e80fac6289c7ead8c85598ed3c0b00d6d
khaledkamal327 <khaledko830@gmail.com>
2026-09-14 03:03:36 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const path = require("path");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-const incomeRoutes = require("./routes/incomeRoutes"); 
 const expenseRoutes = require("./routes/expenseRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
@@ -28,8 +28,8 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/expense", expenseRoutes);
-app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
