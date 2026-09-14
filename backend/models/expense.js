c4cd4804c4f24e59babcb9021c6602732bd30a7f
Menna Tamer <mennatamer751@gmail.com>
2026-09-14 03:17:36 +0300

    Rename 'user' field to 'userId' in ExpenseSchema

--- backend/models/expense.js
+++ backend/models/expense.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     icon: { type: String },
     category: { type: String, required: true },
     amount: { type: Number, required: true },
